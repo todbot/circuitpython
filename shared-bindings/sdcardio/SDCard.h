7e5709f1bbe15ca8e117a90defa90fd7a75ce7af
Bernhard Bablok <bablokb@gmx.de>
2026-06-10 13:48:23 +0200

    support DigitialInOutProtocol for CS of sdcardio.SDCard

--- shared-bindings/sdcardio/SDCard.h
+++ shared-bindings/sdcardio/SDCard.h
@@ -12,7 +12,7 @@
 
 extern const mp_obj_type_t sdcardio_SDCard_type;
 
-void common_hal_sdcardio_sdcard_construct(sdcardio_sdcard_obj_t *self, busio_spi_obj_t *spi, const mcu_pin_obj_t *cs, int baudrate);
+void common_hal_sdcardio_sdcard_construct(sdcardio_sdcard_obj_t *self, busio_spi_obj_t *spi, mp_obj_t cs, int baudrate);
 void common_hal_sdcardio_sdcard_deinit(sdcardio_sdcard_obj_t *self);
 bool common_hal_sdcardio_sdcard_deinited(sdcardio_sdcard_obj_t *self);
 void common_hal_sdcardio_sdcard_check_for_deinit(sdcardio_sdcard_obj_t *self);
