2b88f851c27255a74170b97f00cd1fade3478513
foamyguy <foamyguy@gmail.com>
2026-07-07 09:05:25 -0500

    fix module name typo, enable sdioio for metro rp2040

--- ports/raspberrypi/boards/adafruit_metro_rp2040/pins.c
+++ ports/raspberrypi/boards/adafruit_metro_rp2040/pins.c
@@ -6,6 +6,18 @@
 
 #include "shared-bindings/board/__init__.h"
 
+// Four consecutive data GPIOs for the 4-bit SDIO interface (sdioio.SDCard).
+static const mp_rom_obj_tuple_t sdio_data_tuple = {
+    {&mp_type_tuple},
+    4,
+    {
+        MP_ROM_PTR(&pin_GPIO36),
+        MP_ROM_PTR(&pin_GPIO37),
+        MP_ROM_PTR(&pin_GPIO38),
+        MP_ROM_PTR(&pin_GPIO39),
+    }
+};
+
 static const mp_rom_map_elem_t board_module_globals_table[] = {
     CIRCUITPYTHON_BOARD_DICT_STANDARD_ITEMS
 
@@ -63,6 +75,8 @@ static const mp_rom_map_elem_t board_module_globals_table[] = {
 
     { MP_OBJ_NEW_QSTR(MP_QSTR_SD_CS), MP_ROM_PTR(&pin_GPIO23) },
     { MP_OBJ_NEW_QSTR(MP_QSTR_SDIO_DATA3), MP_ROM_PTR(&pin_GPIO23) },
+
+    { MP_OBJ_NEW_QSTR(MP_QSTR_SDIO_DATA), MP_ROM_PTR(&sdio_data_tuple) },
 
     { MP_ROM_QSTR(MP_QSTR_I2C), MP_ROM_PTR(&board_i2c_obj) },
     { MP_ROM_QSTR(MP_QSTR_STEMMA_I2C), MP_ROM_PTR(&board_i2c_obj) },
